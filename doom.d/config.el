af29dfacac83468a66dc9144353f8ca0fdb19e05
billow <billow.fun@gmail.com>
2022-10-22 19:44:46 +0800

    _

--- doom.d/config.el
+++ doom.d/config.el
@@ -21,9 +21,9 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 21 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 21))
-;;
+(setq doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 21 :weight 'semi-light))
+
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
